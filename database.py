1d0b74e0f57c43e3a74bd0ba7cb42ab805bae061
LuKa-gu <gumilarluka@gmail.com>
2025-04-10 12:32:49 +0000

    Initial commit

---
+++ database.py
@@ -0,0 +1,11 @@
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from sqlalchemy.orm import sessionmaker
+
+DATABASE_URL = "sqlite+aiosqlite:///./test.db"
+
+engine = create_async_engine(DATABASE_URL, echo=True)
+SessionLocal = sessionmaker(
+    engine,
+    expire_on_commit=False,
+    class_=AsyncSession
+)
